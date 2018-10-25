be5c7efb74a7c239b6a1370febb8eaadb987612e
cdrainxv <cdrainxv@users.noreply.github.com>
2018-10-25 09:19:42 -0600

    scaffold posts controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_230540) do
+ActiveRecord::Schema.define(version: 2018_10_25_151801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2018_10_24_230540) do
     t.datetime "updated_at"
     t.integer "blocker_id"
     t.integer "status"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +47,5 @@ ActiveRecord::Schema.define(version: 2018_10_24_230540) do
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
