c2bad6e57799b6c17dfc97b3a982ba5e50cfc68a
cdrainxv <cdrainxv@users.noreply.github.com>
2018-10-26 12:22:03 -0600

    limit likes by the same user to 1

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,13 +4,22 @@ class LikesController < ApplicationController
   before_action :find_post
 
   def create
-    @post.likes.create(user_id: current_user.id)
-    redirect_to user_post_path @post
+    if already_liked?
+      flash[:notice] = 'You have already like this post! \
+      Look for other posts to like.'
+    else
+      @post.likes.create(user_id: current_user.id)
+    end
+    redirect_to user_posts_path
   end
 
   private
 
   def find_post
-    @post = Post.find(param[:post_id])
+    @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
   end
 end
