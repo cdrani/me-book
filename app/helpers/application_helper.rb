93f854fa713941196f5e6b2d005eb3db18a82a2e
cdrainxv <cdrainxv@users.noreply.github.com>
2018-11-01 11:23:45 -0600

    default images
    - setup default avatar icon
    - profile_avatar_select helper to display image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def profile_avatar_select(user)
+    return image_tag user.avatar.variant(resize: '96x96!') if user.avatar.attached?
+
+    image_tag 'default_avatar.png', width: 96, height: 96
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
+  resources :users, only: [:index]
 
   resources :posts do
     resources :comments, only: %i[create destroy]
