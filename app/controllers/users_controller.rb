29aee822add7a59d2125047682ce7e433457b7cf
cdrainxv <cdrainxv@users.noreply.github.com>
2018-11-02 15:05:37 -0600

    setup users controller to send and accept friend requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,37 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_friend, only: %i[add_friend request_friend]
+
   def index
     @not_friends_yet = User.not_my_friends(current_user)
     @pending_friends = current_user.pending_friends
     @requested_friends = current_user.requested_friends
     @friends = current_user.friends
+  end
+
+  def add_friend
+    if current_user.accept_request(@friend)
+      flash[:success] = "#{@friend.user_name} added to friends list"
+      redirect_to posts_path
+    end
+  end
+
+  def request_friend
+    if current_user.friend_request(@friend)
+      flash[:success] = "Friend request sent to #{@friend.user_name}"
+      redirect_to posts_path
+    end
+    console
+  end
+
+  private
+
+  def set_friend
+    @friend = User.find_by(id: params[:id])
+  end
+
+  def friend_params
+    params.permit(:id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :users, only: [:index]
+  resources :users, only: [:index] do
+    post 'add_friend/:id', to: 'users#add_friend', as: :add_friend
+    post 'request_friend/:id', to: 'users#request_friend', as: :request_friend
+  end
 
   resources :posts do
     resources :comments, only: %i[create destroy]
