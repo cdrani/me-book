e3504b8c7079230f03ae74c3bd9d0fd4ff68b479
cdrainxv <cdrainxv@users.noreply.github.com>
2019-01-22 09:39:27 -0700

    update notification table settings

--- db/migrate/20190122162414_create_notifications.rb
+++ db/migrate/20190122162414_create_notifications.rb
@@ -3,7 +3,7 @@
 class CreateNotifications < ActiveRecord::Migration[5.2]
   def change
     create_table :notifications do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, index: true
       t.references :notified_by, index: true
       t.references :post, foreign_key: true, index: true
       t.integer :identifier
