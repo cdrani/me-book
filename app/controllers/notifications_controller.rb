baea0644b84b392da1fb2bf4a33caeac3cff1ab1
cdrainxv <cdrainxv@users.noreply.github.com>
2019-01-22 11:36:41 -0700

    create index view for all notifications

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,11 @@
+class NotificationsController < ApplicationController
+  def index
+    @notifications = current_user.notifications
+  end
+
+  def link_through
+    @notification = Notification.find(params[:id])
+    @notification.update(read: true)
+    redirect_to post_path(@notification.post)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'notifications', to: 'notifications#index'
+  get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :users, only: [:index] do
     post 'add_friend/:id', to: 'users#add_friend', as: :add_friend
