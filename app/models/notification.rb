eb8f275ff1a8e78c653d0cc72a1b2b16a1597f2c
cdrainxv <cdrainxv@users.noreply.github.com>
2019-01-22 09:49:04 -0700

    add validations to Notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 class Notification < ApplicationRecord
+  belongs_to :notified_by, class_name: 'User'
   belongs_to :user
-  belongs_to :subscribe_user
   belongs_to :post
+
+  validates :user_id, presence: true
+  validates :notified_by_id, presence: true
+  validates :post_id, presence: true
+  validates :notice_type, presence: true
 end
