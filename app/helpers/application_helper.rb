6db9f20848fcec4be7f3271436990656f57423f2
cdrainxv <cdrainxv@users.noreply.github.com>
2019-02-04 14:22:24 -0700

    resize default images; add burger and brand to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@
 
 module ApplicationHelper
   def profile_avatar_select(user)
-    return image_tag user.avatar.variant(resize: '96x96!') if user.avatar.attached?
+    return image_tag user.avatar.variant(resize: '128x128!') if user.avatar.attached?
 
-    image_tag 'default_avatar.png', width: 96, height: 96
+    image_tag 'default_avatar.png', width: 128, height: 128 
   end
 end
