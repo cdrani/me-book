b413676543e75b4863f522bfdc1f50480a174e20
cdrainxv <cdrainxv@users.noreply.github.com>
2019-02-04 13:13:43 -0700

    notification styling
    - move _notification partial into shared folder
    - update styling of Notification navbar item dropdown

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,5 +7,6 @@ class NotificationsController < ApplicationController
     @notification = Notification.find(params[:id])
     @notification.update(read: true)
     redirect_to post_path(@notification.post)
+    @notification.delete
   end
 end
