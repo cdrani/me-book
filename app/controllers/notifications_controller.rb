09cbb22ac455716f7403863d84149f792174234e
cdrainxv <cdrainxv@users.noreply.github.com>
2019-02-05 03:29:19 -0700

    fix profile avatar styling and textarea line-height

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.where(read: false)
   end
 
   def link_through
